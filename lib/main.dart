2f8cb211cbba251091f0b9cb4e520c07da11f69b
Youssef Dawoud <41302314+DeathPancake1@users.noreply.github.com>
2022-02-14 19:08:56 +0200

    removed unnecessary dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'dart:math';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
